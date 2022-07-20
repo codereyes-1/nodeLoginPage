d4374795761932c7f5a0849214f3e5798f660797
codereyes-1 <80291124+codereyes-1@users.noreply.github.com>
2022-07-19 21:21:53 -0500

    -hashedpwd to log /register

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// https://www.youtube.com/watch?v=-RCnNyD0L-s
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
@@ -23,20 +24,24 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+app.post(
+  "/register",
+  (req, res) => {
+    // try {
+    // const hashedPassword = bcrypt.hash(req.body.password, 10);
     users.push({
       id: Date.now().toString(),
       name: req.body.name,
       email: req.body.email,
-      password: hashedPassword,
+      password: req.body.password,
     });
     res.redirect("/login");
-  } catch {
-    res.redirect("/register");
+    console.log(users);
+    // } catch {
+    //   res.redirect("/register");
+    // }
   }
-  console.log(users);
-});
+  // console.log(users)
+);
 
 app.listen(3000);
